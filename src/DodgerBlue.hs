0e63132bf119b42947f7b8380d6cb8cd4a326a06
Andrew Browne <brownie@brownie.com.au>
2016-08-23 12:03:22 +0800

    Apply hindent

--- src/DodgerBlue.hs
+++ src/DodgerBlue.hs
@@ -1,37 +1,53 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 module DodgerBlue
-    ( evalDslIO,
-      forkChild,
-      newQueue,
-      writeQueue,
-      tryReadQueue,
-      readQueue,
-      wait,
-      setPulseStatus,
-      CustomDsl) where
+  (evalDslIO
+  ,forkChild
+  ,newQueue
+  ,writeQueue
+  ,tryReadQueue
+  ,readQueue
+  ,wait
+  ,setPulseStatus
+  ,CustomDsl)
+  where
 
-import           Data.Typeable
-import           Control.Monad.Free.Church
-import           DodgerBlue.IO (evalDslIO)
-import           DodgerBlue.Types
+import Data.Typeable
+import Control.Monad.Free.Church
+import DodgerBlue.IO (evalDslIO)
+import DodgerBlue.Types
 
-forkChild :: (Functor d, MonadFree (CustomDsl q d) m) => F (CustomDsl q d) () -> m ()
+forkChild
+    :: (Functor d, MonadFree (CustomDsl q d) m)
+    => F (CustomDsl q d) () -> m ()
 forkChild p = liftF . DslBase $ ForkChild' p ()
 
-newQueue :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a) => m (q a)
+newQueue
+    :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a)
+    => m (q a)
 newQueue = liftF . DslBase $ NewQueue' id
 
-writeQueue :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a) => q a -> a -> m ()
+writeQueue
+    :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a)
+    => q a -> a -> m ()
 writeQueue queue item = liftF . DslBase $ WriteQueue' queue item ()
 
-tryReadQueue :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a) => q a -> m (Maybe a)
+tryReadQueue
+    :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a)
+    => q a -> m (Maybe a)
 tryReadQueue queue = liftF . DslBase $ TryReadQueue' queue id
 
-readQueue :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a) => q a -> m a
+readQueue
+    :: (Functor d, MonadFree (CustomDsl q d) m, Typeable a)
+    => q a -> m a
 readQueue queue = liftF . DslBase $ ReadQueue' queue id
 
-wait :: (Functor d, MonadFree (CustomDsl q d) m) => Int -> m ()
+wait
+    :: (Functor d, MonadFree (CustomDsl q d) m)
+    => Int -> m ()
 wait seconds = liftF . DslBase $ Wait' seconds ()
 
-setPulseStatus :: (Functor d, MonadFree (CustomDsl q d) m) => Bool -> m ()
+setPulseStatus
+    :: (Functor d, MonadFree (CustomDsl q d) m)
+    => Bool -> m ()
 setPulseStatus active = liftF . DslBase $ SetPulseStatus' active ()
--- src/DodgerBlue/InMemoryQueues.hs
+++ src/DodgerBlue/InMemoryQueues.hs
@@ -5,69 +5,90 @@
 
 module DodgerBlue.InMemoryQueues
   (Queues
-  , Queue
-  , emptyQueues
-  , newQueue
-  , writeToQueue
-  , tryReadFromQueue
-  ) where
+  ,Queue
+  ,emptyQueues
+  ,newQueue
+  ,writeToQueue
+  ,tryReadFromQueue)
+  where
 
-import           Data.Dynamic
+import Data.Dynamic
 import qualified Data.Sequence as Seq
-import           Data.Sequence ((<|),ViewR(..))
+import Data.Sequence ((<|), ViewR(..))
 import qualified Data.Map.Strict as Map
-import           Data.Map.Strict (Map)
+import Data.Map.Strict (Map)
 
-data Queues = Queues {
-  queuesNextIndex :: Int,
-  queuesQueueMap :: Map Int (Maybe Dynamic)
-}
-  deriving (Show)
+data Queues = Queues
+    { queuesNextIndex :: Int
+    , queuesQueueMap :: Map Int (Maybe Dynamic)
+    } deriving ((Show))
 
-newtype Queue a = Queue { unQueue :: Int }
-  deriving (Show)
+newtype Queue a = Queue
+    { unQueue :: Int
+    } deriving ((Show))
 
 emptyQueues :: Queues
-emptyQueues = Queues {
-  queuesNextIndex = 0,
-  queuesQueueMap = mempty }
+emptyQueues = 
+    Queues
+    { queuesNextIndex = 0
+    , queuesQueueMap = mempty
+    }
 
 newQueue :: Queues -> (Queue a, Queues)
-newQueue Queues{..} =
-  let
-    queueKey = Queue { unQueue = queuesNextIndex }
-    queues = Queues {
-        queuesNextIndex = queuesNextIndex + 1,
-        queuesQueueMap = Map.insert queuesNextIndex Nothing queuesQueueMap }
-  in (queueKey, queues)
+newQueue Queues{..} = 
+    let queueKey = 
+            Queue
+            { unQueue = queuesNextIndex
+            }
+        queues = 
+            Queues
+            { queuesNextIndex = queuesNextIndex + 1
+            , queuesQueueMap = Map.insert
+                  queuesNextIndex
+                  Nothing
+                  queuesQueueMap
+            }
+    in (queueKey, queues)
 
-writeToQueue :: Typeable a => Queues -> Queue a -> a -> Queues
-writeToQueue queues@Queues{..} Queue{..} item =
-  let
-    q = queuesQueueMap Map.! unQueue
-    updatedQueue = addItem q
-  in
-    queues { queuesQueueMap = Map.insert unQueue (Just updatedQueue) queuesQueueMap }
+writeToQueue
+    :: Typeable a
+    => Queues -> Queue a -> a -> Queues
+writeToQueue queues@Queues{..} Queue{..} item = 
+    let q = queuesQueueMap Map.! unQueue
+        updatedQueue = addItem q
+    in queues
+       { queuesQueueMap = Map.insert unQueue (Just updatedQueue) queuesQueueMap
+       }
   where
     addItem Nothing = toDyn $ Seq.singleton item
-    addItem (Just dyn) =
-      let
-        items = fromDyn dyn (error "InMemoryQueues.writeToQueue Invalid format for queue")
-      in toDyn $ item <| items
+    addItem (Just dyn) = 
+        let items = 
+                fromDyn
+                    dyn
+                    (error
+                         "InMemoryQueues.writeToQueue Invalid format for queue")
+        in toDyn $ item <| items
 
-tryReadFromQueue :: Typeable a => Queues -> Queue a -> (Maybe a, Queues)
-tryReadFromQueue queues@Queues{..} Queue{..} =
-  let
-    q = queuesQueueMap Map.! unQueue
-    (maybeItem, updatedQueue) = tryRead q
-  in (maybeItem, queues { queuesQueueMap = Map.insert unQueue updatedQueue queuesQueueMap })
+tryReadFromQueue
+    :: Typeable a
+    => Queues -> Queue a -> (Maybe a, Queues)
+tryReadFromQueue queues@Queues{..} Queue{..} = 
+    let q = queuesQueueMap Map.! unQueue
+        (maybeItem,updatedQueue) = tryRead q
+    in ( maybeItem
+       , queues
+         { queuesQueueMap = Map.insert unQueue updatedQueue queuesQueueMap
+         })
   where
     tryRead Nothing = (Nothing, Nothing)
-    tryRead (Just dyn) =
-      let
-        items = fromDyn dyn (error "InMemoryQueues.writeToQueue Invalid format for queue")
-        (item, queue') = tryReadSeq items
-      in (item, toDyn <$> queue')
+    tryRead (Just dyn) = 
+        let items = 
+                fromDyn
+                    dyn
+                    (error
+                         "InMemoryQueues.writeToQueue Invalid format for queue")
+            (item,queue') = tryReadSeq items
+        in (item, toDyn <$> queue')
     tryReadSeq (Seq.viewr -> Seq.EmptyR) = (Nothing, Nothing)
     tryReadSeq (Seq.viewr -> xs :> x) = (Just x, Just xs)
     tryReadSeq _ = error "match error on Seq"
